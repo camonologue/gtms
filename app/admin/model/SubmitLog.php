45e931d579aee99e6d4953ebdb417182407df59e
camonologue <2339695474@qq.com>
2022-08-25 16:47:55 +0800

    论文管理(论文详情，批改记录）

---
+++ app/admin/model/SubmitLog.php
@@ -0,0 +1,25 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\model;
+
+use app\common\model\BaseModel;
+use think\Model;
+
+/**
+ * @mixin \think\Model
+ */
+class SubmitLog extends BaseModel
+{
+    // 表名
+    protected $name = 'treatise_submit_log';
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = '';
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id')->joinType('LEFT');
+    }
+}
